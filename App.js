2ab5adb5e2b8ca914ab57f6c46e77172279fb4b9
kkaniap <xkaniax@gmail.com>
2023-07-31 19:15:18 +0200

    Login page created

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import LoginPage from './screens/LoginPage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>kania</Text>
-      <StatusBar style="auto" />
+    <View style={styles.rootContainer}>
+      <StatusBar style="light" />
+
+      <LoginPage />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00301C',
     alignItems: 'center',
     justifyContent: 'center',
   },
